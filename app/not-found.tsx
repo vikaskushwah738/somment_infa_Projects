34f09487b2e070884632d7c6bdff5cd4eea98272
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-21 18:18:53 +0530

    not fount

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="pb-10 text-2xl px-5 text-blue-400">  Oooh-on! The thing you’re looking for might be on Mars. NASA’s working on it 🚀 </h1>
+      <h1 className="pb-10 text-2xl px-5 text-blue-400 ">  Oooh-on! The thing you’re looking for might be on Mars. NASA’s working on it 🚀 </h1>
       <Link href="/" className="border-2 border-blue-300 p-1 rounded-xl px-4 hover:bg-blue-300 hover:text-gray-100">
         Go Back Home
       </Link>
