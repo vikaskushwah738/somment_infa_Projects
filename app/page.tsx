620b11c60c92130e57c2a779540b0f9a2b388c84
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-25 15:41:30 +0530

    minner chages

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,32 @@
 import HeroSection from "@/components/HeroSection";
 import CarouselHome from "@/components/HomeCarousel";
-
 import OurClient from "@/components/OurClient";
 import Services from "@/components/Services";
 import WhyCooiseUs from "@/components/WhyChooseUs";
-
+import highway from '@/public/highway.png'
+import highway_2 from '@/public/highways-2.png'
+import highway_3 from '@/public/highways-3.png'
 export default function Home() {
   return (
       <main className="lg:pt-28 md:pt-24 pt-20">
        <CarouselHome items={
         [
           {
-            src: `https://media.istockphoto.com/id/504743680/photo/highway-interchange-aerial-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=AxX8KpWd0rwBbQERsPcl2aIAZ6EQigCkBw7dfqqG3f0=`,
+            src:"https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361050/highway_iisgej.png",
             title: "Network Survey Vehicles (NSV)",
             type: "image"
           },
           {
-            src: `https://media.istockphoto.com/id/483800159/photo/highway-in-mumbai.jpg?s=612x612&w=0&k=20&c=pX62yvO00u4p8n7AwC4QbAvj7svWhkWevasJ8gIDt44=`,
+            src: `https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361050/highways-2_hboh9n.png`,
             title: "Network Survey Vehicles (NSV)",
             type: "image"
           },
           {
-            src: `https://media.istockphoto.com/id/504743680/photo/highway-interchange-aerial-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=AxX8KpWd0rwBbQERsPcl2aIAZ6EQigCkBw7dfqqG3f0=`,
+            src: `https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png`,
             title: "Network Survey Vehicles (NSV)",
             type: "image"
           },
-          {
-            src: `https://plus.unsplash.com/premium_photo-1664547606209-fb31ec979c85?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGlnaHdheXN8ZW58MHx8MHx8fDA%3D`,
-            title: "Network Survey Vehicles (NSV)",
-            type: "image"
-          },
-          {
-            src: `https://media.istockphoto.com/id/1279856502/photo/road.webp?a=1&b=1&s=612x612&w=0&k=20&c=stywcPRLrkPbXC1aGqzR1P4XMJH31PG4dnueIY4dsN4=`,
-            title: "Network Survey Vehicles (NSV)",
-            type: "image"
-          },
+          
         ]
        }
        />
