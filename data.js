ea7de6e633bbd7316a63fbb58bade38283cbf2cd
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-25 15:40:46 +0530

    minner chagweed

---
+++ data.js
@@ -0,0 +1,45 @@
+const dummyData = [
+    {
+      imageSrc: 'https://res.cloudinary.com/dopolikju/image/upload/v1732529362/nsv-sommet_wtrqr1.png',
+      heading: 'Network survey vehicle (NSV)',
+      description: 'This is a short description of card 1.',
+      url: 'https://example.com/card1',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Falling Weight Deflectometer (FWD)',
+      description: 'This is a short description of card 2.',
+      url: 'https://example.com/card2',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'BBD Survey',
+      description: 'This is a short description of card 3.',
+      url: 'https://example.com/card3',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Retro-reflectivity Survey',
+      description: 'This is a short description of card 4.',
+      url: 'https://example.com/card4',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Ground Penetrating Radar (GPR)',
+      description: 'This is a short description of card 5.',
+      url: 'https://example.com/card5',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Traffic Surveys',
+      description: 'This is a short description of card 5.',
+      url: 'https://example.com/card5',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Automatic Traffic Counter & Classifier (ATCC)',
+      description: 'This is a short description of card 5.',
+      url: 'https://example.com/card5',
+    },
+  ];
+  export default dummyData;
