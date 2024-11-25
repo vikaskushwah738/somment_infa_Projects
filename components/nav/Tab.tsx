0249082d05e540e6e8544206e94d3cddf734c02a
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-25 18:22:39 +0530

    minner changes

--- components/nav/Tab.tsx
+++ components/nav/Tab.tsx
@@ -1,12 +1,3 @@
-// import { NavLinks } from "@/types/testimonials"
-// import Link from "next/link"
-// export const Tab = ({title, path} : NavLinks ) => {
-//   return (
-//     <li>
-//         <Link className="text-gray-900 hover:underline" href={path}>{title}</Link>
-//     </li>
-//   )
-// }
 import { NavLinks } from "@/types/testimonials";
 import Link from "next/link";
 
