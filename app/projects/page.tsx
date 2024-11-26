200890c3419a26ff94477bd3c84c0e4161258113
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-26 17:47:18 +0530

    updates projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Projects = () => {
+  return (
+    <main className="h-screen flex justify-center items-center lg:pt-28 md:pt-24 pt-20">
+        <h1 className="lg:text-4xl md:text-3xl text-2xl text-blue-950">Comming Soon</h1>
+    </main>
+  )
+}
+
+export default Projects
