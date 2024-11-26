61a1782b840386393bf6d25a66747ceb23f12fa3
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-26 09:43:16 +0530

    adding textimonilas

---
+++ components/Testimonial.tsx
@@ -0,0 +1,13 @@
+
+
+const Testimonial = () => {
+  return (
+    <section className="lg:px-20 md:px-10 px-5">
+     <h2 className="lg:text-4xl md:text-3xl text-2xl font-medium">What Our Customers Are Saying</h2>
+      <h3>Testimonial</h3>
+     
+    </section>
+  )
+}
+
+export default Testimonial
---
+++ data/services-card.js
@@ -0,0 +1,32 @@
+export default const dummyData = [
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Card Title 1',
+      description: 'This is a short description of card 1.',
+      url: 'https://example.com/card1',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Card Title 2',
+      description: 'This is a short description of card 2.',
+      url: 'https://example.com/card2',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Card Title 3',
+      description: 'This is a short description of card 3.',
+      url: 'https://example.com/card3',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Card Title 4',
+      description: 'This is a short description of card 4.',
+      url: 'https://example.com/card4',
+    },
+    {
+      imageSrc: 'https://res.cloudinary.com/dfbm8in4c/image/upload/v1732361058/highways-3_dfg8dp.png',
+      heading: 'Card Title 5',
+      description: 'This is a short description of card 5.',
+      url: 'https://example.com/card5',
+    },
+  ];
