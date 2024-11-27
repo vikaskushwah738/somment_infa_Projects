9a53163a2ea29fe28fd3639b6bf06090c7232221
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-27 15:16:29 +0530

    adding layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import services from '@/public/tech-city.png'
+import SideBar from './components/SideBar';
+export default function Layout({ children }: any) {
+    return (
+        <main className='lg:pt-28 md:pt-24 pt-20 flex'>
+            <SideBar />
+            {children}
+        </main>
+    )
+}
