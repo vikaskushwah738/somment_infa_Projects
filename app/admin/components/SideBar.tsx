dc65a14999ba7bf33720b8ed18c6301b82e335ae
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-27 15:16:00 +0530

    adding side bar in admin page

---
+++ app/admin/components/SideBar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function SideBar() {
+   
+    return (
+        <aside className="lg:w-[25%] pt-20 lg:h-[900px] h-auto w-full border-r  lg:px-3 md:px-10 px-5">
+     
+           
+
+            
+        </aside>
+    )
+}
