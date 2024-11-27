f39484965c4eef7088fa8823de587aa16af0d8dd
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-27 09:27:08 +0530

    minner updteds

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Admin = () => {
+  return (
+    <main className="h-screen flex justify-center items-center lg:pt-28 md:pt-24 pt-20">
+        <h1>Admin</h1>
+    </main>
+  )
+}
+
+export default Admin
