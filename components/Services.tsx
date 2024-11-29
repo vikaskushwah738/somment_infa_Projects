84b46c4e7f9bc1b09a059ee1ed7b54eb6c552ef3
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-29 12:06:32 +0530

    minner chages

--- components/Services.tsx
+++ components/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
       <p className="text-base pt-3 text-center lg:px-8 md:px-5 px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium corrupti cum praesentium possimus ut, ullam veritatis nulla vel autem iusto eveniet</p>
       <div className="flex gap-5 justify-center items-center flex-wrap lg:pt-14 md:pt-10 pt-5">
         {dummyData.map((data: any) => (<ServicesCard key={data.heading} {...data} />))}
-        <Link  href='services' className="py-2 px-5 rounded-xl hover:text-blue-800 hover:border-blue-800 border-2  hover:bg-gray-50 cursor-pointer bg-blue-800 text-gray-50">
+        <Link  href='services' className="py-2 px-5 rounded-xl hover:text-blue-400 hover:border-blue-400 border-2  hover:bg-gray-50 cursor-pointer bg-blue-400 text-gray-50">
           learn more
         </Link>
       </div>
