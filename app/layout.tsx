eea58927b4d4df88d313542333438724dfd5b1ed
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-29 14:15:27 +0530

    minner chage add toast massages

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Footer from "@/components/Footer";
 import { Navbar } from "@/components/nav/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Navbar/>   
         {children}   
        <Footer/>  
+       <Toaster />
       </body>
     </html>
   );
