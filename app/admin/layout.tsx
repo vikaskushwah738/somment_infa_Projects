3ba8ff92d6a5aeb8c7f1c1f5f8d7397f2f630680
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-29 12:00:39 +0530

    adding toast in layout page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import services from '@/public/tech-city.png'
 import SideBar from './components/SideBar';
+import { Toaster } from 'react-hot-toast';
 export default function Layout({ children }: any) {
     return (
         <main className='lg:pt-28 md:pt-24 pt-20 flex'>
             <SideBar />
             {children}
+            <Toaster />
         </main>
     )
 }
