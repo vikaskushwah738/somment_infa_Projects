161ec3ac540f1129e1bfb7e2e4b0a77541140b43
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-29 12:05:55 +0530

    minner chaghes

--- app/admin/components/SideBar.tsx
+++ app/admin/components/SideBar.tsx
@@ -1,13 +1,52 @@
 import Link from "next/link";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { FaThList, FaDatabase } from "react-icons/fa";
+import { MdPostAdd , MdUpdate} from "react-icons/md";
 
 export default function SideBar() {
-   
+    const link = [
+        {
+            name: 'Dashboard',
+            link: '/admin',
+            icon: <AiOutlineDashboard />
+        },
+        {
+            name: 'Careers Form',
+            link: '/admin/careersinfo',
+            icon: <FaDatabase />
+        },
+        {
+            name: 'News',
+            link: '/admin/news-updates',
+            icon: <MdPostAdd />
+        },
+        {
+            name: 'Category',
+            link: '/admin/addyears',
+            icon: <MdUpdate />
+        },
+        {
+            name: 'Projects',
+            link: '/admin/projectsUpdates',
+            icon: <FaThList />
+        }
+    ]
     return (
-        <aside className="lg:w-[25%] pt-20 lg:h-[900px] h-auto w-full border-r  lg:px-3 md:px-10 px-5">
-     
-           
+        <aside className="w-[25%] pt-20 lg:h-[900px] h-auto  border-r lg:px-3 md:px-10 px-5 bg-blue-50">
+            <h1 className="pb-3 text-2xl font-medium flex justify-center">Admin</h1>
+            <ul className="flex flex-col gap-6 w-full">
+                {link.map((item) => {
+                    return <Link
+                        key={item.name}
+                        href={item.link}
+                        className="flex items-center font-semibold text-center gap-2 px-4 py-2 rounded-full bg-gradient-to-r from-blue-100 via-blue-200 to-blue-400">
+                        <span className="text-blue-800 text-xl">{item.icon}</span>
+                        <samp>{item.name}</samp>
+                    </Link>
+                })}
+            </ul>
 
-            
+
         </aside>
     )
 }
