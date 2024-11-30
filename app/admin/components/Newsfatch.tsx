6696cda804d2596867d704ce94a5f5c1af520dff
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-30 15:04:47 +0530

    news fatch

---
+++ app/admin/components/Newsfatch.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { NewsData } from "@prisma/client";
+import { useState, useEffect } from "react";
+import AdminNewsCard from "./NewsCArd";
+
+const AdminNewsFatch = () => {
+  const [news, setNews] = useState<NewsData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/news-upload');
+        if (!response.ok) {
+          const errorDetails = await response.text();
+          throw new Error(`Failed to fetch news: ${errorDetails}`);
+        }
+        
+        const data = await response.json();
+        setNews(data);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
+  if (loading) return <div className=' h-screen justify-center flex items-center'>Loading...</div>;
+  if (error) return <div className=' h-screen justify-center flex items-center'>Error: {error}</div>;
+  return (
+    <>
+     {news.map((news, indx) => <AdminNewsCard  key={indx} {...news}/>)}
+    </>
+  )
+}
+
+export default AdminNewsFatch
