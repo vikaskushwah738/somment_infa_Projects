dd400146fce6801a016565030391ba0e433a9edb
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-30 17:33:21 +0530

    minner changes

--- app/api/news-upload/[slug]/route.ts
+++ app/api/news-upload/[slug]/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request, { params }: { params: { slug: string } }): Promise<NextResponse> {
     try {
         const { slug } = params;
-
+        
         // Fetch the news post using the unique slug with findFirst
         const news_post = await prisma.newsData.findFirst({
             where: { slug },  // Querying by slug
@@ -15,10 +15,40 @@ export async function GET(req: Request, { params }: { params: { slug: string } }
             return NextResponse.json({ message: 'News post not found' }, { status: 404 });
         }
 
-        // Return the news post data if found
         return NextResponse.json(news_post, { status: 200 });
     } catch (error) {
         console.error('Something went wrong:', error);
         return NextResponse.json({ message: 'Could not retrieve news post' }, { status: 500 });
     }
 }
+
+
+
+export async function DELETE(req: Request, { params }: { params: { slug: string } }): Promise<NextResponse> {
+    try {
+        const { slug } = params;
+
+         // First, checking if the post exists
+         const existingPost = await prisma.newsData.findFirst({
+            where: { slug },  // Querying by slug
+        });
+
+        // If the post doesn't exist, return a 404 error
+        if (!existingPost) {
+            return NextResponse.json({ message: 'News post not found' }, { status: 404 });
+        }
+
+        // Fetching the news post using the unique slug with findFirst
+        const deletePost = await prisma.newsData.delete({
+            where: { slug },  // Querying by slug
+        });
+         console.log("deleted post sucessfully", deletePost)
+       
+        return NextResponse.json({ message: 'News post deleted successfully' }, { status: 200 });
+    } catch (error :any) {
+        console.error('Something went wrong:', error);
+        return NextResponse.json({ message: 'Could not deleted news post' }, { status: 500 });
+    }
+}
+
+
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,7 +3,7 @@ import NewsFatch from "@/components/NewsFatch"
 const News = () => {
   
     return (
-      <main className=" lg:pt-28 md:pt-24 pt-20 lg:px-10 md:px-10 px-5">
+      <main className=" lg:pt-28 md:pt-24 pt-20 lg:px-20 md:px-10 px-5">
           <h1 className="lg:text-4xl md:text-3xl text-2xl text-blue-950 pt-10">News</h1>
           <section className=" flex-wrap flex gap-5 py-10">
              <NewsFatch/>
