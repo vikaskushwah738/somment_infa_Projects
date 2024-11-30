0d3a86f3a60749e74a4029feab96e61a7bca973a
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-30 11:59:24 +0530

    new componet

---
+++ types/newsTypes.ts
@@ -0,0 +1,11 @@
+export type NewsProps = {
+    imageUrl: string;
+    category: string;
+    content: string;
+    title: string;
+    id: string;
+    slug: string;
+    description: string;
+    publicId: string;
+    createdAt: Date;
+}
