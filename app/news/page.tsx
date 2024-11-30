bc581428b3ca50f9e4bc0cb0086fb81f58a0a3d3
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-11-30 11:59:37 +0530

    news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,13 @@
+import NewsFatch from "@/components/NewsFatch"
+
 const News = () => {
+  
     return (
-      <main className="h-screen flex justify-center items-center lg:pt-28 md:pt-24 pt-20">
-          <h1 className="lg:text-4xl md:text-3xl text-2xl text-blue-950">Comming Soon</h1>
+      <main className=" lg:pt-28 md:pt-24 pt-20 lg:px-10 md:px-10 px-5">
+          <h1 className="lg:text-4xl md:text-3xl text-2xl text-blue-950 pt-10">News</h1>
+          <section className=" flex-wrap flex gap-5 py-10">
+             <NewsFatch/>
+          </section>
       </main>
     )
   }
