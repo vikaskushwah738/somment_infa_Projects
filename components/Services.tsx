5bb2f9da0a9b335a08a4e63759cfca102fb912f8
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-02 16:38:15 +0530

    vikas

--- components/Services.tsx
+++ components/Services.tsx
@@ -7,7 +7,7 @@ const Services = () => {
       <h2 className="lg:text-4xl md:text-3xl text-2xl text-center">Our Engineering Consultancy Services</h2>
       <p className="text-base pt-3 text-center lg:px-8 md:px-5 px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium corrupti cum praesentium possimus ut, ullam veritatis nulla vel autem iusto eveniet</p>
       <div className="flex gap-5 justify-center items-center flex-wrap lg:pt-14 md:pt-10 pt-5">
-        {dummyData.map((data: any) => (<ServicesCard key={data.heading} {...data} />))}
+        {dummyData.map((data) => (<ServicesCard key={data.heading} {...data} />))}
         <Link  href='services' className="py-2 px-5 rounded-xl hover:text-blue-400 hover:border-blue-400 border-2  hover:bg-gray-50 cursor-pointer bg-blue-400 text-gray-50">
           learn more
         </Link>
