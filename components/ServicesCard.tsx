31754c651fc5fbab0feaebdb86f5f36f773816bf
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-02 16:37:02 +0530

    minner chages

--- components/ServicesCard.tsx
+++ components/ServicesCard.tsx
@@ -6,7 +6,18 @@ const ServicesCard = ({ imageSrc, heading, description, url }: CardProps) => {
   return (
     <Link href={url} className="text-blue-950 cursor-pointer pt-5">
       <div className="w-64 h-96  rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105 ">
-        <Image  src={imageSrc} alt={heading} className="w-full h-52 object-cover " />
+        <div className='relative h-56 w-full'>
+          <Image
+            src={imageSrc}
+            alt={heading}
+            fill
+            style={{ objectFit: 'fill' }}
+            className="object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+        </div>
+
         <div className="pt-5 px-5">
           <h2 className="text-lg font-semibold mb-2">{heading}</h2>
           <p className="text-gray-600 ">{description}</p>
